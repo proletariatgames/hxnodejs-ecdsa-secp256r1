06944b35ae7d90d8590edb2c376dfab07bf47b82
ChetRippo <bdavis1000@gmail.com>
2020-04-07 14:06:58 -0400

    add externs for ecdsa-secp256r1

---
+++ src/js/npm/ECDSA.hx
@@ -0,0 +1,12 @@
+package js.npm.ecdsa-secp256r1;
+
+@:jsRequire("ecdsa-secp256r1")
+extern class ECDSA {
+  static function generateKey():ECDSA;
+
+  function asPublic():ECDSA
+
+  function toJWK():{kty:String, crv:String, x:String, y:String, ?d:String}
+
+  function sign(message:String, ?format:String):String;
+}
