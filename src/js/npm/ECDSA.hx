5a6da87bd90724faf1e5e8f0d23841a45944e385
ChetRippo <bdavis1000@gmail.com>
2020-04-07 14:11:01 -0400

    fix compile issues

--- src/js/npm/ECDSA.hx
+++ src/js/npm/ECDSA.hx
@@ -4,9 +4,9 @@ package js.npm.ecdsa-secp256r1;
 extern class ECDSA {
   static function generateKey():ECDSA;
 
-  function asPublic():ECDSA
+  function asPublic():ECDSA;
 
-  function toJWK():{kty:String, crv:String, x:String, y:String, ?d:String}
+  function toJWK():{kty:String, crv:String, x:String, y:String, ?d:String};
 
   function sign(message:String, ?format:String):String;
 }
